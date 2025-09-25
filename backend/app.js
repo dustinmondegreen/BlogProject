bc6eaf0d5247d8e67967002a1c3cafeea5828d56
dustinmondegreen <antowilliam1029@gmail.com>
2025-09-25 14:37:31 +0700

    Export default app after function definition

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,9 @@
 import express from "express"
-import mongoose from "mongoose"
-import {MongoDB_URI} from "./config/config.js"
+import mongooseConnect from "./utils/mongooseConnect.js"
 
 const app = express()
 
 app.use(express.json())
-
-const mongooseConnect = async () => {
-    try{
-        const connect = await mongoose.connect(MongoDB_URI)
-        console.log('MongoDB Atalas Connected Succesfully')
-    } catch ( error ) {
-        console.log(`MongoDB Atlas Connection error: ${error}`)
-    }
-}
 
 mongooseConnect();
 
---
+++ backend/utils/mongooseConnect.js
@@ -0,0 +1,13 @@
+import {MongoDB_URI} from "../config/config.js"
+import mongoose from "mongoose"
+
+const mongooseConnect = async () => {
+    try{
+        const connect = await mongoose.connect(MongoDB_URI)
+        console.log('MongoDB Atalas Connected Succesfully')
+    } catch ( error ) {
+        console.log(`MongoDB Atlas Connection error: ${error}`)
+    }
+}
+
+export default mongooseConnect
