21c591e3aa58334c2a706f222b8ec0ce50c80eb0
dustinmondegreen <antowilliam1029@gmail.com>
2025-11-18 18:17:44 +0700

    Add express-validator for registration route

--- src/app.js
+++ src/app.js
@@ -4,17 +4,7 @@ import passport from 'passport';
 const app = express();
 
 app.use(express.json())
-
+ 
 initdb();
-
-// app.get('/', (req, res) => {
-//     console.log('root received')
-//     return res.status(200).json({message: "Received"})
-// })
-
-// app.post('/api/v1/users', (req, res) => {
-//     console.log(req.body);
-//     return res.status(200).send({message: 'received'});
-// })
 
 export default app
