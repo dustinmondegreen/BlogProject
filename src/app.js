2c0fcb6d5e48008f3a18ed9f0496f9d7203ecf17
dustinmondegreen <antowilliam1029@gmail.com>
2025-11-18 15:43:52 +0700

    Install passport and express-session dependencies for authentication

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import initdb from '../db/initdb.js';
-
+import passport from 'passport';
 const app = express();
 
+app.use(express.json())
+
 initdb();
+
+// app.get('/', (req, res) => {
+//     console.log('root received')
+//     return res.status(200).json({message: "Received"})
+// })
+
+// app.post('/api/v1/users', (req, res) => {
+//     console.log(req.body);
+//     return res.status(200).send({message: 'received'});
+// })
 
 export default app
