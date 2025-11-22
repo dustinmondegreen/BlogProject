992e243ccb5e9204d89015f467a63859955c181e
dustinmondegreen <antowilliam1029@gmail.com>
2025-11-22 10:21:19 +0700

    Add password hashing using bcrypt

--- src/app.js
+++ src/app.js
@@ -2,17 +2,23 @@ import express from 'express';
 import initdb from '../db/initdb.js';
 import { db } from '../db/initdb.js';
 import { validateUser } from '../middleware/validation.js';
+import bcrypt from 'bcrypt'
 
 const app = express();
 app.use(express.json());
 
 initdb();
 
-app.post('/api/v1/users', validateUser, (req, res) => {
+app.post('/api/v1/users', validateUser, async (req, res) => {
     try{
         const {email, username, password} = req.body;
+
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const insert = db.prepare(`INSERT INTO users(email, username, password_hash) VALUES (?, ?, ?)`);
-        const info = insert.run(email, username, password)
+
+        const info = insert.run(email, username, passwordHash)
+
         return res.status(201).send({message: `${info.lastInsertRowid}`});
     } catch (error) {
         return res.status(400).send({message: `${error}`})
